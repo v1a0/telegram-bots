f87c9a295bf42dd4ab81295b799c0ec7b3edfa17
V1A0 <54343363+V1A0@users.noreply.github.com>
2020-02-24 13:10:52 +0700

    TGB v0.1
    Just for fun

---
+++ fun/gaybot/README.md
@@ -0,0 +1,44 @@
+# Telegram Gay Bot (TGB) v 0.1   ![AIOgramm](https://camo.githubusercontent.com/17cc9a743b4e951d7977528e6ec6da7f2a9c8bbf/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f74656c656772616d2d61696f6772616d2d626c75652e7376673f7374796c653d666c61742d737175617265)  ![Telegram BOT API](https://camo.githubusercontent.com/63f0f2836a5a94e7452fd23c57492522b4102332/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f54656c656772616d253230426f742532304150492d342e352d626c75652e7376673f7374796c653d666c61742d737175617265266c6f676f3d74656c656772616d) ![License MIT](https://camo.githubusercontent.com/2c1ceb131587d49e46ff93acd6e3990b9efed3fb/68747470733a2f2f696d672e736869656c64732e696f2f707970692f6c2f61696f6772616d2e7376673f7374796c653d666c61742d737175617265)
+
+```
+██████╗ ██╗███████╗ ██████╗██╗      █████╗ ██╗███╗   ███╗███████╗██████╗
+██╔══██╗██║██╔════╝██╔════╝██║     ██╔══██╗██║████╗ ████║██╔════╝██╔══██╗
+██║  ██║██║███████╗██║     ██║     ███████║██║██╔████╔██║█████╗  ██████╔╝
+██║  ██║██║╚════██║██║     ██║     ██╔══██║██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+██████╔╝██║███████║╚██████╗███████╗██║  ██║██║██║ ╚═╝ ██║███████╗██║  ██║
+╚═════╝ ╚═╝╚══════╝ ╚═════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝
+```
+
+# Warning! Profanity words!
+## By their very nature these lists of swear words contain material many will find offensive. But that's kind of the point. It's nothing bad to be gay, it's just kidding. If you think it's outrageous and unacceptable, please contact to repository owner.
+
+```
+██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗██╗  ██╗
+╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝╚██╗██╔╝
+ ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝  ╚███╔╝
+ ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗  ██╔██╗
+██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗██╔╝ ██╗
+```
+Funny bot to count Gay Points, then more some user using G-words then more G-point it have.<br>
+To get list of most gayest users send 'whogay' or 'ктогей'.<br>
+Examples: 
+
+<img src="https://raw.githubusercontent.com/V1A0/Telegram-bots/Telegram-bots/fun/gaybot/pic/gay.png"
+    alt="example-screenshot"
+    style="float: left;" />
+
+<img src="https://raw.githubusercontent.com/V1A0/Telegram-bots/Telegram-bots/fun/gaybot/pic/who.png"
+    alt="example-screenshot"
+    style="float: left;" />
+
+---
+
+Also you can "ОСУЖДАТЬ" (rus. lil. "condemn") some user, if you'll do, bot will notify all users in chat about that.<br>
+Example:
+
+<img src="https://raw.githubusercontent.com/V1A0/Telegram-bots/Telegram-bots/fun/gaybot/pic/osujdau.png"
+    alt="example-screenshot"
+    style="float: left;" />
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ fun/gaybot/bot.py
@@ -0,0 +1,161 @@
+"""
+Telegram Gay Bot v0.1
+"""
+
+import logging
+import json
+from gaywords import *
+from aiogram import Bot, Dispatcher, executor, types
+import os
+
+"""
+ENTER YOUR API_TOKEN HERE v v v v v v v v 
+"""
+API_TOKEN = os.environ['T_GAY_BOT_TOKEN']
+"""
+ENTER YOUR API_TOKEN HERE ^ ^ ^ ^ ^ ^ ^ ^ 
+                                            API_TOKEN = "111111111:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+"""
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# JSON FILES IMPORT
+gay_rate = json.load(open('gay_rate.json', 'r'))
+persons = json.load(open('persons.json', 'r'))
+
+
+# Save gay-rate
+def save_rate():
+    json.dump(gay_rate, open('gay_rate.json', 'w'))
+
+
+# Save information about user
+def save_persons():
+    json.dump(persons, open('persons.json', 'w'))
+
+
+# Authorization decorator
+def auth(func):
+    async def firewall(message):
+        """
+        # Easy user filter
+
+        if message['from']['id'] != *some user id*:
+            return await message.reply("Access Denied", reply=False)
+        """
+        return await func(message)
+
+    return firewall
+
+
+# /start and /help echos
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+# Got any message script
+@dp.message_handler()
+@auth
+async def main(message: types.Message):
+    # Gay Points counter
+    async def add_gp(message: types.Message):
+
+        try:
+            # IF Message is Replayed
+
+            tar = dict(message.reply_to_message['from'])  # All target information
+            tar_id = tar['id'].__str__()  # Get target id
+            cargs = message.text.split(' ')  # Command's args
+            points = int(cargs[1])  # Gay Points
+
+            # Adding GayPoints
+            temp = {tar_id: gay_rate.get(tar_id) + points if gay_rate.get(tar_id) is not None else points}
+            gay_rate.update(temp)
+
+            # Info Message
+            await message.reply(f" {message.reply_to_message['from']['first_name']}"
+                                f" (@{message.reply_to_message['from']['username']})"
+                                f" got {points} GayPoints!", reply=True)
+
+        except:
+            # IF Message is not Replayed
+
+            tar = dict(message.from_user)  # All target information
+            tar_id = tar['id'].__str__()  # Get target id
+            points = 1  # Gay Points
+
+            # Adding GayPoints
+            temp = {tar_id: gay_rate.get(tar_id) + points if gay_rate.get(tar_id) is not None else points}
+            gay_rate.update(temp)
+
+            # Info Message
+            await message.reply(f" {message.from_user['first_name']}"
+                                f" (@{message.from_user['username']}) "
+                                f" got 1 GayPoint!", reply=True)
+
+        persons.update({tar_id: tar})   # Update user info
+
+        #   Save all
+        save_persons()
+        save_rate()
+
+    # Get TOP
+    async def gaylist():
+        _result = ''
+        r = 1
+        for g in sorted(gay_rate, key=lambda item: item[1], reverse=False):
+            try:
+                _result += f"{r}. {persons[g]['first_name']} :  {gay_rate[g]} GC\n"
+                r += 1
+
+            except KeyError:
+                pass
+
+        # Info Message
+        await message.reply(f"- - - - - TOP GAYS - - - - - -\n"
+                            f"{_result}"
+                            f"- - - - - - - - - - - - - - - - - - - - "
+                            f"")
+
+    # OSYJDAU MOD
+    async def osyjdau(message: types.Message):
+        try:
+            # IF Message is Replayed
+            tar = dict(message.from_user)
+
+            # tar_id = tar['id'].__str__()
+            # Info Message
+            await message.reply(f" {message.reply_to_message['chat']['first_name']}"
+                                # f" (@{message.reply_to_message['chat']['username']})"
+                                f" осуждает пользователя"
+                                f" {message.reply_to_message['from']['first_name']}"
+                                # f" (@{message.reply_to_message['from']['username']})!!!"
+                                , reply=True)
+
+
+        except:
+            # IF Message is not Replayed
+            pass
+
+    # MSG -> LIST of WORDS
+    msg = message.text.lower().strip(""",./!';[]{}-=\+|()"'""").split(' ')
+
+    # IF some G-word in message
+    if [w for w in msg for g in Gwords if w == g]:
+        await add_gp(message)
+
+    # IF some G-list-word in message
+    elif [w for w in msg for g in whogay if w == g]:
+        await gaylist()
+
+    # IF "ОСУЖДАЮ" in message
+    elif 'осуждаю' in msg:
+        await osyjdau(message)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
